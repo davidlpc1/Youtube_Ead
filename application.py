471fba97abf88d22ffeb9560ffffb0acc65fcc7d
Davi Lucas <davilucasdlcreator@gmail.com>
2020-12-26 19:45:31 -0300

    Register implemented

--- application.py
+++ application.py
@@ -1,6 +1,15 @@
-"""POWERSHELL $env:FLASK_APP = "application.py" """
-"""CMD set FLASK_APP=application.py"""
-"""Linux,Mac export FLASK_APP=application.py"""
+#POWERSHELL 
+#  $env:FLASK_APP = "application.py"  
+#  $env:FLASK_ENV = "development" 
+#  $env:FLASK_DEBUG=0
+#CMD 
+#  set FLASK_APP=application.py 
+#  set FLASK_ENV=development 
+#  set FLASK_DEBUG=0
+#Linux,Mac 
+#  export FLASK_APP=application.py 
+#  export FLASK_ENV=development 
+#  export FLASK_DEBUG=0
 
 import os
 
@@ -39,23 +48,8 @@ db = SQL("sqlite:///EAD.db")
 @app.route("/")
 @login_required
 def index():
-    """Show portfolio of stocks"""
+    """Show videos,playlists,level"""
     return apology("TODO")
-
-
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """Buy shares of stock"""
-    return apology("TODO")
-
-
-@app.route("/history")
-@login_required
-def history():
-    """Show history of transactions"""
-    return apology("TODO")
-
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -69,11 +63,11 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return render_template("login.html",message_error="You must provide username")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return render_template("login.html",message_error="You must provide password")
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
@@ -81,7 +75,7 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return render_template("login.html",message_error="Invalid username/password")
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -105,25 +99,57 @@ def logout():
     return redirect("/")
 
 
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    """Get stock quote."""
-    return apology("TODO")
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        #Get information of Form and verify if any of them is Null
+        username = request.form.get("username")
+        password = request.form.get("password")
+        passwordAgain = request.form.get("password_again")
+        # Ensure username was submitted
+        if not username:
+            return render_template("register.html", message_error="You must provide username")
+        # Ensure password was submitted
+        elif not password or not passwordAgain:
+            return render_template("register.html", message_error="You must provide password")
 
+        #Ensure passwords are equal
+        elif password != passwordAgain:
+            return render_template("register.html", message_error="The passwords are not equal")
 
-@app.route("/sell", methods=["GET", "POST"])
+        #Verify if user already exists
+        userAlreadyExists = db.execute("SELECT * FROM users WHERE username = :username",
+        username=username)
+
+        if(len(userAlreadyExists) == 1):
+            return render_template("register.html", message_error="That name is already in use")
+
+        # Registring user
+        db.execute("INSERT INTO users (username,hash,level) VALUES(:username,:hashpassword,1); ",
+        username=username,hashpassword=generate_password_hash(password))
+
+        # Taking user id to the session
+        user = db.execute('SELECT * FROM users WHERE username = :username',username=username)[0]
+        session["user_id"] = user["id"]
+        session['messageOfIndexPage'] = 'Registered!'
+        return redirect('/')
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+@app.route("/perfil",methods=["GET", "POST"])
 @login_required
-def sell():
-    """Sell shares of stock"""
-    return apology("TODO")
-
+def perfil():
+    """Show videos,playlists,level"""
+    if request.method == "POST":
+        return render_template("perfil.html", message_error="Method Post in progress")
+    aboutUser = db.execute('SELECT * FROM users WHERE id = :userId',userId=session["user_id"])[0]
+    print('--------------------------------------------------')
+    print('aboutUser:         ',aboutUser)
+    print('--------------------------------------------------')
+    return render_template("perfil.html",aboutUser=aboutUser)
 
 def errorhandler(e):
     """Handle error"""
